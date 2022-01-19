1522988f6e07bd878a97407055bde64002f9fec9
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-01-19 12:46:40 +0700

    add asset layout page home - doctor

---
+++ src/assets/dummy/index.js
@@ -0,0 +1,17 @@
+import DummyUser from "./dummyUser.png";
+import DummyDoctor1 from "./dummyDoctor1.png";
+import DummyDoctor2 from "./dummyDoctor2.png";
+import DummyDoctor3 from "./dummyDoctor3.png";
+import DummyNews1 from "./dummyNews1.png";
+import DummyNews2 from "./dummyNews2.png";
+import DummyNews3 from "./dummyNews3.png";
+
+export {
+  DummyUser,
+  DummyDoctor1,
+  DummyDoctor2,
+  DummyDoctor3,
+  DummyNews1,
+  DummyNews2,
+  DummyNews3
+};
--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,5 +1,15 @@
 import ILLogo from "./logo.svg";
 import ILGetStarted from "./bgGetStarted.png";
 import ILNullPhoto from "./null-photo.png";
+import ILCatUmum from "./ic-cat-dokter-umum.svg";
+import ILCatObat from "./ic-cat-obat.svg";
+import ILCatPsikiater from "./ic-cat-psikiater.svg";
 
-export { ILLogo, ILGetStarted, ILNullPhoto };
+export {
+  ILLogo,
+  ILGetStarted,
+  ILNullPhoto,
+  ILCatUmum,
+  ILCatPsikiater,
+  ILCatObat
+};
