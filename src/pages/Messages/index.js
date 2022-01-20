93d8d8033e97c2496012d04272787092eb9072a2
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-01-20 23:19:36 +0700

    add component messages

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,12 +1,37 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import ListDoctor from "../../components/molecules/ListDoctor";
+import { colors } from "../../utils";
 
 export default function Messages() {
   return (
-    <View>
-      <Text>Messages page</Text>
+    <View style={styles.page}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Messages</Text>
+        <ListDoctor />
+        <ListDoctor />
+        <ListDoctor />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.secondary,
+    flex: 1
+  },
+  content: {
+    backgroundColor: colors.white,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginLeft: 16
+  }
+});
