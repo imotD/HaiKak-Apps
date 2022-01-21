b6d2d745d72896c9d488dc2fe0f9732132cd4841
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-01-21 15:10:30 +0700

    change icon and custom name app

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const MainApp = () => {
 
 export default function Router() {
   return (
-    <Stack.Navigator initialRouteName="MainApp">
+    <Stack.Navigator initialRouteName="Splash">
       <Stack.Screen
         name="Splash"
         component={Splash}
