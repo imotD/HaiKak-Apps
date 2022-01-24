116e1ca9d8a39e8b7168e98215ff421553c302bf
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-01-24 15:12:43 +0700

    install and setup  firebase

---
+++ src/config/Fire/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBX0ZpyZ_daPH0QAecAfiJZD-o8ZcHSxd4",
+  authDomain: "hikak-4add4.firebaseapp.com",
+  projectId: "hikak-4add4",
+  storageBucket: "hikak-4add4.appspot.com",
+  messagingSenderId: "703722220122",
+  appId: "1:703722220122:web:72581393eece85bee8c663"
+};
+
+const Fire = initializeApp(firebaseConfig);
+
+export default Fire;
